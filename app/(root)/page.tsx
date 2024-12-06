7ee563b7847649a3f8930d1cc1a8865a58099f3e
Mihail Bivol <biv@yahoo.com>
2024-12-06 09:29:13 +0000

    Hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,15 @@
 export default function Home() {
   return (
     <>
-      <h1>Home</h1>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch your Stratup <br /> Connect with entrepreneurs.
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit ideas, Vote on pitches, and Get noticed in virtual
+          competitions!
+        </p>
+      </section>
     </>
   );
 }
