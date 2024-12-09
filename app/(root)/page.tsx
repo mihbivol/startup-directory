df565070be42c731e4bf734b2e2bd662d2603ad9
Mihail Bivol <biv@yahoo.com>
2024-12-09 10:58:11 +0000

    The content cards

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,18 @@
+import StartupCard from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
 
 export default async function Home({searchParams}:{searchParams: Promise<{query?: string}>}) {
 const query = (await searchParams).query;
+
+const posts = [{
+  _createdAt: new Date(),
+  views: 55,
+  author: {_id: 1, name: 'Mihai'},
+  description: 'This is a description',
+  image: 'a url',
+  category: 'Robots',
+  title: 'We robots',
+}]
   return (
     <>
       <section className="pink_container">
@@ -13,6 +24,22 @@ const query = (await searchParams).query;
           competitions!
         </p>
         <SearchForm query={query}/>
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : `All Startups`}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts ?.length > 0 ? (
+            posts.map((post: StartUpCardType, index: number) => (
+              <StartupCard key={post ?._id} post={post}/>
+            ))
+          ) : ( 
+          <p className="no-results">No Startup found.</p>
+          )}
+        </ul>
+
       </section>
     </>
   );
