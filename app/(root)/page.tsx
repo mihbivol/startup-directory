387489fac463e99ef3040735902bcc28f2dc3a60
Mihail Bivol <biv@yahoo.com>
2024-12-12 11:55:26 +0000

    Fetching data & type safety

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,23 +1,17 @@
-import StartupCard from "@/components/StartupCard";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
+import { client } from "@/sanity/lib/client";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
 export default async function Home({searchParams}:{searchParams: Promise<{query?: string}>}) {
 const query = (await searchParams).query;
+const posts = await client.fetch(STARTUPS_QUERY);
 
-const posts = [{
-  _createdAt: new Date(),
-  views: 55,
-  author: {_id: 1, name: 'Mihai'},
-  description: 'This is a description',
-  image: 'a url',
-  category: 'Robots',
-  title: 'We robots',
-}]
   return (
     <>
       <section className="pink_container">
         <h1 className="heading">
-          Pitch your Stratup <br /> Connect with entrepreneurs.
+          Pitch your Startup <br /> Connect with entrepreneurs.
         </h1>
         <p className="sub-heading !max-w-3xl">
           Submit ideas, Vote on pitches, and Get noticed in virtual
@@ -32,7 +26,7 @@ const posts = [{
 
         <ul className="mt-7 card_grid">
           {posts ?.length > 0 ? (
-            posts.map((post: StartUpCardType, index: number) => (
+            posts.map((post: StartupTypeCard, index: number) => (
               <StartupCard key={post ?._id} post={post}/>
             ))
           ) : ( 
