93aea2baa01614ad1d014329353e8ceb4ec01984
Mihail Bivol <biv@yahoo.com>
2024-12-12 12:14:57 +0000

    Cache & Live API

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,11 @@
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
-import { client } from "@/sanity/lib/client";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 
 export default async function Home({searchParams}:{searchParams: Promise<{query?: string}>}) {
 const query = (await searchParams).query;
-const posts = await client.fetch(STARTUPS_QUERY);
+const {data: posts} = await sanityFetch({query: STARTUPS_QUERY});
 
   return (
     <>
@@ -33,8 +33,8 @@ const posts = await client.fetch(STARTUPS_QUERY);
           <p className="no-results">No Startup found.</p>
           )}
         </ul>
-
       </section>
+      <SanityLive/>
     </>
   );
 }
